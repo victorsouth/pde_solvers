09dcc0279af9fbb05cd7012b8137087724f58055
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2023-05-07 12:26:38 +0300

    fix: UTF-8

--- testing/test_quick.h
+++ testing/test_quick.h
@@ -1,17 +1,17 @@
-#pragma once
+п»ї#pragma once
 
 
-/// @brief Разработка метода прямых разностей по [Leonard 1979]
+/// @brief Р Р°Р·СЂР°Р±РѕС‚РєР° РјРµС‚РѕРґР° РїСЂСЏРјС‹С… СЂР°Р·РЅРѕСЃС‚РµР№ РїРѕ [Leonard 1979]
 TEST(UpstreamDifferencing, Develop)
 {
-    // Профиль переменных
+    // РџСЂРѕС„РёР»СЊ РїРµСЂРµРјРµРЅРЅС‹С…
     typedef templated_layer<0, 1> target_var_t;
     typedef templated_layer<1, 0> specific_data_t;
 
-    // Слой: переменных Vars + сколько угодно служебных Specific
+    // РЎР»РѕР№: РїРµСЂРµРјРµРЅРЅС‹С… Vars + СЃРєРѕР»СЊРєРѕ СѓРіРѕРґРЅРѕ СЃР»СѓР¶РµР±РЅС‹С… Specific
     typedef composite_layer_t<target_var_t, specific_data_t> layer_t;
 
-    // Упрощенное задание трубы - 50км, с шагом разбиения для расчтной сетки 1км, диаметром 700мм
+    // РЈРїСЂРѕС‰РµРЅРЅРѕРµ Р·Р°РґР°РЅРёРµ С‚СЂСѓР±С‹ - 50РєРј, СЃ С€Р°РіРѕРј СЂР°Р·Р±РёРµРЅРёСЏ РґР»СЏ СЂР°СЃС‡С‚РЅРѕР№ СЃРµС‚РєРё 1РєРј, РґРёР°РјРµС‚СЂРѕРј 700РјРј
     simple_pipe_properties simple_pipe;
     simple_pipe.length = 50e3;
     simple_pipe.diameter = 0.7;
@@ -19,13 +19,13 @@ TEST(UpstreamDifferencing, Develop)
 
     PipeProperties pipe = PipeProperties::build_simple_pipe(simple_pipe);
 
-    vector<double> Q(pipe.profile.getPointCount(), 0.5); // задаем по трубе расход 0.5 м3/с
+    vector<double> Q(pipe.profile.getPointCount(), 0.5); // Р·Р°РґР°РµРј РїРѕ С‚СЂСѓР±Рµ СЂР°СЃС…РѕРґ 0.5 Рј3/СЃ
     PipeQAdvection advection_model(pipe, Q);
 
-    //Текущий и предыдущий слой, каждый из которых представляет собой composite_layer (Var+Specific)
+    //РўРµРєСѓС‰РёР№ Рё РїСЂРµРґС‹РґСѓС‰РёР№ СЃР»РѕР№, РєР°Р¶РґС‹Р№ РёР· РєРѕС‚РѕСЂС‹С… РїСЂРµРґСЃС‚Р°РІР»СЏРµС‚ СЃРѕР±РѕР№ composite_layer (Var+Specific)
     custom_buffer_t<layer_t> buffer(2, 10);
 
-    //Получение текущего/предыдущего слоя
+    //РџРѕР»СѓС‡РµРЅРёРµ С‚РµРєСѓС‰РµРіРѕ/РїСЂРµРґС‹РґСѓС‰РµРіРѕ СЃР»РѕСЏ
     layer_t& prev = buffer.previous();
     layer_t& next = buffer.current();
 
@@ -35,13 +35,13 @@ TEST(UpstreamDifferencing, Develop)
     prev.vars.cell_double[0] = vector<double>(prev.vars.cell_double[0].size(), 850);
 
     auto& next_spec = std::get<0>(next.specific);
-    auto& F = next_spec.point_double[0]; // потоки на границах ячеек
+    auto& F = next_spec.point_double[0]; // РїРѕС‚РѕРєРё РЅР° РіСЂР°РЅРёС†Р°С… СЏС‡РµРµРє
     auto& U = prev.vars.cell_double[0];
     auto& U_new = next.vars.cell_double[0]; //
 
-    double dt = 60; // 1 минута
+    double dt = 60; // 1 РјРёРЅСѓС‚Р°
 
-    // Расчет потоков на границе на основе граничных условий
+    // Р Р°СЃС‡РµС‚ РїРѕС‚РѕРєРѕРІ РЅР° РіСЂР°РЅРёС†Рµ РЅР° РѕСЃРЅРѕРІРµ РіСЂР°РЅРёС‡РЅС‹С… СѓСЃР»РѕРІРёР№
     double v_in = advection_model.getEquationsCoeffs(0, U[0]);
     double v_out = advection_model.getEquationsCoeffs(F.size() - 1, U[U.size() - 1]);
     if (v_in >= 0) {
@@ -52,10 +52,10 @@ TEST(UpstreamDifferencing, Develop)
     }
     
 
-    // Расчет потоков на границе по правилу донорской ячейки
+    // Р Р°СЃС‡РµС‚ РїРѕС‚РѕРєРѕРІ РЅР° РіСЂР°РЅРёС†Рµ РїРѕ РїСЂР°РІРёР»Сѓ РґРѕРЅРѕСЂСЃРєРѕР№ СЏС‡РµР№РєРё
     for (size_t cell = 0; cell < U.size(); ++cell) {
         double u = U[cell];
-        double v_cell = advection_model.getEquationsCoeffs(cell, u);//не совсем корректно, скорость в ячейке берется из скорости на ее левой границе
+        double v_cell = advection_model.getEquationsCoeffs(cell, u);//РЅРµ СЃРѕРІСЃРµРј РєРѕСЂСЂРµРєС‚РЅРѕ, СЃРєРѕСЂРѕСЃС‚СЊ РІ СЏС‡РµР№РєРµ Р±РµСЂРµС‚СЃСЏ РёР· СЃРєРѕСЂРѕСЃС‚Рё РЅР° РµРµ Р»РµРІРѕР№ РіСЂР°РЅРёС†Рµ
         if (v_cell > 0) {
             size_t right_point = cell + 1;
             double v_right = advection_model.getEquationsCoeffs(right_point, u);
@@ -70,10 +70,10 @@ TEST(UpstreamDifferencing, Develop)
 
     const auto& grid = advection_model.get_grid();
     for (size_t cell = 0; cell < U.size(); ++cell) {
-        double dx = grid[cell + 1] - grid[cell]; // ячейки обычно одинаковой длины, но мало ли..
+        double dx = grid[cell + 1] - grid[cell]; // СЏС‡РµР№РєРё РѕР±С‹С‡РЅРѕ РѕРґРёРЅР°РєРѕРІРѕР№ РґР»РёРЅС‹, РЅРѕ РјР°Р»Рѕ Р»Рё..
         U_new[cell] = U[cell] + dt / dx * ((F[cell] - F[cell + 1]));
     }
    
-    //Движение на слой вперед 
+    //Р”РІРёР¶РµРЅРёРµ РЅР° СЃР»РѕР№ РІРїРµСЂРµРґ 
     //buffer.advance(+1);
 }
