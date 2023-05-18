5eca38af8b7c4fc351f55c784f4b41836ff2a680
Nikita1526 <56439362+Nikita1526@users.noreply.github.com>
2023-05-18 16:59:12 +0300

    QUICK_solver + plotter

--- testing/test_quick.h
+++ testing/test_quick.h
@@ -12,6 +12,16 @@ struct upstream_fv_solver_traits
         0, 0> specific_layer;
 };
 
+/// @brief Описание типов данных для метода конечных объемов на основе QUICK 
+template <size_t Dimension>
+struct quick_fv_solver_traits
+{
+    typedef templated_layer<0, Dimension/*переменные - ячейки*/, 0, 0, 0, 0> var_layer_data;
+    typedef templated_layer<Dimension /*потоки F*/, 0,
+        0, 0,
+        0, 0> specific_layer;
+};
+
 /// @brief Солвер на основе upstream differencing, только для размерности 1!
 /// [Leonard 1979]
 class upstream_fv_solver {
@@ -107,6 +117,130 @@ public:
     }
 };
 
+double quick_border_approximation(double U_L, double U_C, double U_R)
+{
+    double Ub_linear = (U_C + U_R) / 2;
+    double Ub_cubic_correction = -(U_L + U_R - 2 * U_C) / 8;
+    return Ub_linear + Ub_cubic_correction;
+}
+
+/// @brief Солвер на основе QUICK, только для размерности 1!
+/// [Leonard 1979]
+class quick_fv_solver {
+public:
+    typedef typename quick_fv_solver_traits<1>::var_layer_data var_layer_data;
+    typedef typename quick_fv_solver_traits<1>::specific_layer specific_layer;
+    typedef typename fixed_system_types<1>::matrix_type matrix_type;
+    typedef typename fixed_system_types<1>::var_type vector_type;
+protected:
+    /// @brief ДУЧП
+    pde_t<1>& pde;
+    /// @brief Сетка, полученная от ДУЧП
+    const vector<double>& grid;
+    /// @brief Количество точек сетки
+    const size_t n;
+    /// @brief Предыдущий слой переменных
+    const var_layer_data& prev_vars;
+    /// @brief Новый (рассчитываемый) слой переменных
+    var_layer_data& curr_vars;
+    /// @brief Предыдущий специфический слой (сейчас не нужен! нужен ли в будущем?)
+    const specific_layer& prev_spec;
+    /// @brief Текущий специфический слой
+    specific_layer& curr_spec;
+public:
+    /// @brief Конструктор для буфера в котором простой слой:
+    /// когда в слое только один блок целевых переменных и один блок служебных данных
+    /// Из буфера берется current() и previous()
+    /// @param pde ДУЧП
+    /// @param buffer Буфер слоев
+    quick_fv_solver(pde_t<1>& pde,
+        custom_buffer_t<composite_layer_t<var_layer_data, specific_layer>>& buffer)
+        : quick_fv_solver(pde, buffer.previous(), buffer.current())
+    {}
+
+    /// @brief Конструктор для простых слоев - 
+    /// когда в слое только один блок целевых переменных и один блок служебных данных
+    /// @param pde ДУЧП
+    /// @param prev Предыдущий слой (уже рассчитанный)
+    /// @param curr Следующий (новый), для которого требуется сделать расчет
+    quick_fv_solver(pde_t<1>& pde,
+        const composite_layer_t<var_layer_data, specific_layer>& prev,
+        composite_layer_t<var_layer_data, specific_layer>& curr)
+        : pde(pde)
+        , grid(pde.get_grid())
+        , n(pde.get_grid().size())
+        , prev_vars(prev.vars)
+        , curr_vars(curr.vars)
+        , prev_spec(std::get<0>(prev.specific))
+        , curr_spec(std::get<0>(curr.specific))
+    {
+
+    }
+    /// @brief Расчет шага
+    /// @param dt Заданный период времени
+    /// @param u_in Левое граничное условие
+    /// @param u_out Правое граничное условие
+    void step(double dt, double u_in, double u_out) {
+        auto& F = curr_spec.point_double[0]; // потоки на границах ячеек
+        const auto& U = prev_vars.cell_double[0];
+        auto& U_new = curr_vars.cell_double[0];
+
+        // Расчет потоков на границе на основе граничных условий
+        double v_in = pde.getEquationsCoeffs(0, U[0]);
+        double v_out = pde.getEquationsCoeffs(F.size() - 1, U[U.size() - 1]);
+        if (v_in >= 0) {
+            F.front() = v_in * u_in;
+        }
+        if (v_out <= 0) {
+            F.back() = v_out * u_out;
+        }
+
+        double v_pipe = pde.getEquationsCoeffs(0, U[0]);//не совсем корректно, скорость в ячейке берется из скорости на ее левой границе
+        // Расчет потоков на границе по правилу QUICK
+        if (v_pipe >= 0) {
+            for (size_t cell = 0; cell < U.size(); ++cell) {
+                size_t right_border = cell + 1;
+                double Vb = v_pipe; // предположили, что скорость на границе во всех точках трубы одна и та же
+                double Ub;
+                if (cell == 0) {
+                    Ub = quick_border_approximation(U[cell], U[cell], U[cell + 1]); // костыль U_L = U_C
+                }
+                else if (cell == U.size() - 1) {
+                    Ub = quick_border_approximation(U[cell - 1], U[cell], U[cell]); // костыль U_R = U_C
+                }
+                else {
+                    Ub = quick_border_approximation(U[cell - 1], U[cell], U[cell + 1]); // честный расчет
+                }
+                F[right_border] = Ub * Vb;
+            }
+        }
+        else {
+            for (size_t cell = 0; cell < U.size(); ++cell) {
+                size_t left_border = cell;
+                double Vb = v_pipe; // предположили, что скорость на границе во всех точках трубы одна и та же
+                double Ub;
+                if (cell == 0) {
+                    Ub = quick_border_approximation(U[cell + 1], U[cell], U[cell]); // костыль U_L = U_C
+                }
+                else if (cell == U.size() - 1) {
+                    Ub = quick_border_approximation(U[cell], U[cell], U[cell - 1]); // костыль U_R = U_C
+                }
+                else {
+                    Ub = quick_border_approximation(U[cell + 1], U[cell], U[cell - 1]); // честный расчет
+                }
+                F[left_border] = Ub * Vb;
+            }
+
+        }
+
+        for (size_t cell = 0; cell < U.size(); ++cell) {
+            double dx = grid[cell + 1] - grid[cell]; // ячейки обычно одинаковой длины, но мало ли..
+            U_new[cell] = U[cell] + dt / dx * ((F[cell] - F[cell + 1]));
+        }
+
+    }
+};
+
 /// @brief Тесты для солвера upstream_fv_solver
 class UpstreamDifferencing : public ::testing::Test {
 protected:
@@ -135,6 +269,42 @@ protected:
         pipe = PipeProperties::build_simple_pipe(simple_pipe);
 
         Q = vector<double> (pipe.profile.getPointCount(), 0.5);
+        advection_model = std::make_unique<PipeQAdvection>(pipe, Q);
+        buffer = std::make_unique<custom_buffer_t<layer_t>>(2, pipe.profile.getPointCount());
+
+        layer_t& prev = buffer->previous();
+        prev.vars.cell_double[0] = vector<double>(prev.vars.cell_double[0].size(), 850);
+    }
+};
+
+/// @brief Тесты для солвера quick_fv_solver
+class QUICK : public ::testing::Test {
+protected:
+    // Профиль переменных
+    typedef quick_fv_solver_traits<1>::var_layer_data target_var_t;
+    typedef quick_fv_solver_traits<1>::specific_layer specific_data_t;
+
+    // Слой: переменных Vars + сколько угодно служебных Specific
+    typedef composite_layer_t<target_var_t, specific_data_t> layer_t;
+protected:
+    /// @brief Параметры трубы
+    PipeProperties pipe;
+    /// @brief Профиль расхода
+    vector<double> Q;
+    std::unique_ptr<PipeQAdvection> advection_model;
+    std::unique_ptr<custom_buffer_t<layer_t>> buffer;
+protected:
+
+    /// @brief Подготовка к расчету для семейства тестов
+    virtual void SetUp() override {
+        // Упрощенное задание трубы - 50км, с шагом разбиения для расчтной сетки 1км, диаметром 700мм
+        simple_pipe_properties simple_pipe;
+        simple_pipe.length = 50e3;
+        simple_pipe.diameter = 0.7;
+        simple_pipe.dx = 1000;
+        pipe = PipeProperties::build_simple_pipe(simple_pipe);
+
+        Q = vector<double>(pipe.profile.getPointCount(), 0.5);
         advection_model = std::make_unique<PipeQAdvection>(pipe, Q);
         buffer = std::make_unique<custom_buffer_t<layer_t>>(2, pipe.profile.getPointCount());
 
@@ -180,8 +350,6 @@ TEST_F(UpstreamDifferencing, Develop)
 
 }
 
-
-
 /// @brief Пример вывода в файл через
 TEST_F(UpstreamDifferencing, UseCaseStepDensity)
 {
@@ -214,11 +382,36 @@ TEST_F(UpstreamDifferencing, UseCaseStepDensity)
     output.close();
 }
 
-double quick_border_approximation(double U_L, double U_C, double U_R)
+/// @brief Пример вывода в файл через
+TEST_F(QUICK, UseCaseStepDensity)
 {
-    double Ub_linear = (U_C + U_R) / 2;
-    double Ub_cubic_correction = - (U_L + U_R - 2 * U_C) / 8;
-    return Ub_linear + Ub_cubic_correction;
+    string path = prepare_test_folder();
+    std::ofstream output(path + "output.csv");
+
+    vector<double> rho_in{ 860, 860, 860, 860, 860, 860, 860, 860, 860, 860 };
+    double rho_out = 870;
+    double dt = 60; // 1 минута
+    double t = 0;
+
+    for (size_t index = 0; index < rho_in.size(); ++index) {
+        if (index == 0) {
+            layer_t& prev = buffer->previous();
+            prev.vars.print(t, output);
+        }
+
+        t += dt;
+
+        quick_fv_solver solver(*advection_model, *buffer);
+        solver.step(dt, rho_in[index], rho_out);
+
+        layer_t& next = buffer->current();
+        next.vars.print(t, output);
+
+        buffer->advance(+1);
+
+    }
+    output.flush();
+    output.close();
 }
 
 /// @brief Разработка метода QUICK по [Leonard 1979]
