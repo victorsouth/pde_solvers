1bd85b7bb64748c5afb256acef0ed4c09a5710b5
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2023-11-25 15:21:56 +0300

    refactor: Вдогонку

--- testing/test_diffusion.h
+++ testing/test_diffusion.h
@@ -43,46 +43,6 @@ double get_C_x_t(double ts, double xs, double Pe, double h, const VectorXd& C_ta
 }
 
 
-double get_C_x_t2(double ts, double xs, double Pe, double h, size_t N, const vector<double>& input)
-{
-    double C = 0;
-    if (N > input.size())
-    {
-        throw std::runtime_error("Wrong input length");
-    }
-
-    for (size_t i = 0; i < N - 1; i++)
-    {
-        double tau = (i + 1) * h;
-        double tau_p = (i)*h;
-        double I2 = function_under_integral(ts, tau, xs, input[i], Pe);
-        double I1 = function_under_integral(ts, tau_p, xs, input[i], Pe);
-        C = C + (I2 + I1) * h / 2; // метод трапеций
-        //C = C + I1 * h; // метод прямоугольников, только для 
-    }
-    C = C * sqrt(Pe) / (2 * sqrt(M_PI));
-    return C;
-}
-
-
-double calc_diffusive_transport(double t, double x, double delta_t, 
-    double v, double L, double K,
-    const vector<double>& input)
-{
-    double T = L / v;
-    double Pe = v * L / K;
-
-    double ts = t / T; // безразмерное время
-    double xs = x / L; // безразмерная координата
-    
-    double delta_ts = delta_t / T; // шаг интегрирования в безразмерном времени
-
-    size_t N = static_cast<size_t>(t / delta_t + 0.5);
-
-    return get_C_x_t2(ts, xs, Pe, delta_ts, N, input);
-}
-
-
 /// Эксперимент с изменением входной концентрации 
 VectorXd compute_C_experiment(VectorXd t, const VectorXd& x, double Pe, size_t T, size_t L, double del, size_t len)
 {
@@ -339,6 +299,73 @@ public:
     {
 
     }
+private:
+    /// @brief Подинтегральное выражение
+    /// Дидковская Новый метод расчета многопродуктовых магистральных
+    /// трубопроводов 2018 ф-ла #7
+    /// @param ts Верхний предел интеграла (безразмерное время)
+    /// @param tau Текущее безразмерное время
+    /// @param xs Безразмерная координата, для которой в конечном итоге будет построена функция параметра от безразмерного времени
+    /// @param C_tau Значение параметра для текущего безразмерного времени
+    /// @param Pe Число Пекле Pe = v*L/K
+    /// @return Значение подинтегрального выражения
+    static double function_under_integral2(double ts, double tau, double xs, double C_tau, double Pe)
+    {
+        //Исходная формула
+        double R = (xs / (pow(ts - tau, 1.5))) * (exp((-Pe / 4) * (pow(xs - (ts - tau), 2) / (ts - tau)))) * C_tau;
+        return R;
+    }
+
+    /// @brief Расчет величины целевого параметра для фиксированного момента времени для заданной координаты
+    /// Численное интегрирование формулы (7) из 
+    /// Дидковская Новый метод расчета многопродуктовых магистральных трубопроводов 2018
+    /// @param ts Заданное время в безразмерной форме
+    /// @param xs Заданная координата в безразмерной форме
+    /// @param Pe Число Пекле
+    /// @param h Шаг расчета интеграла по безразмерному времени
+    /// @param N Количество точек краевых условий на входе, используемых в расчете
+    /// @param input Краевые условия на входе
+    /// @return Значение целевого параметра в точке (ts, xs)
+    static double get_C_x_t2(double ts, double xs, double Pe, double h, size_t N, const vector<double>& input)
+    {
+        double C = 0;
+        if (N > input.size())
+        {
+            throw std::runtime_error("Wrong input length");
+        }
+
+        for (size_t i = 0; i < N - 1; i++)
+        {
+            double tau = (i + 1) * h;
+            double tau_p = (i)*h;
+            double I2 = function_under_integral2(ts, tau, xs, input[i], Pe);
+            double I1 = function_under_integral2(ts, tau_p, xs, input[i], Pe);
+            C = C + (I2 + I1) * h / 2; // метод трапеций
+            //C = C + I1 * h; // метод прямоугольников, только для 
+        }
+        C = C * sqrt(Pe) / (2 * sqrt(M_PI));
+        return C;
+    }
+
+
+    static double calc_diffusive_transport(double t, double x, double delta_t,
+        double v, double L, double K,
+        const vector<double>& input)
+    {
+        double T = L / v;
+        double Pe = v * L / K;
+
+        double ts = t / T; // безразмерное время
+        double xs = x / L; // безразмерная координата
+
+        double delta_ts = delta_t / T; // шаг интегрирования в безразмерном времени
+
+        size_t N = static_cast<size_t>(t / delta_t + 0.5);
+
+        return get_C_x_t2(ts, xs, Pe, delta_ts, N, input);
+    }
+
+
 public:
     static double calc_diffusion_coefficient(
         const pipe_properties_t& pipe, const oil_parameters_t& oil, double v
@@ -352,7 +379,15 @@ public:
         return K;
     }
 
-    /// @brief Расчет движения партии для заданных моментов времени и заданных координат
+    /// @brief Расчет временного ряда параметра на выходе трубопровода при движении партий
+    /// Поскольку расчет идет очень медленно, предусмотрена возможность 
+    /// задания произвольных моментов времени для выходных параметров (метод это позволяет)
+    /// Коэффициент продольного перемешивания K зависит от скорости, поэтому считается тут внутри
+    /// @param t_output Моменты времени, для которых считается параметр на выходе трубопровода
+    /// @param delta_t Период дискретизации входного временного ряда, он же используется в интеграле
+    /// @param input Временной ряд параметра на входе
+    /// @param v Скорость потока
+    /// @return Выходной временной ряд параметра для моментов времени t_output
     vector<double> solve(
         const vector<double>& t_output,
         double delta_t, const vector<double>& input,
@@ -378,7 +413,7 @@ public:
 };
 
 /// @brief Проверка расчета концентрации на выходе трубы, сравнение со старой моделью
-TEST(Refactor, Test)
+TEST(DiffusionSolver, UseCase)
 {
     auto simple_pipe = simple_pipe_properties::sample_section();
     auto pipe = pipe_properties_t::build_simple_pipe(simple_pipe);
