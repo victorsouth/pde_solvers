63d49c10cd7b0bda4c9b53490cbe92b33826047a
Егор Тараканчиков <egor200050@gmail.com>
2023-12-14 11:44:45 +0300

    research part 3 graphs iteration

---
+++ research/2023-12-diffusion-of-advection/research.m
@@ -0,0 +1,71 @@
+clc
+clear all
+currentDirectory = pwd;
+
+[upperPath, ~, ~] = fileparts(currentDirectory);
+[upperPath, ~, ~] = fileparts(upperPath);
+
+n=0.05;
+
+relativePath = fullfile('research_out', 'DiffusionOfAdvection','CompareQuickestDiffusion');
+
+for i = 1 : 19
+    n_str = num2str(n);
+    fname{i} = strcat('output Cr=',n_str,'.csv');
+    graphname{i} = strcat('Cr = ',n_str);
+    n = n + 0.05;
+end
+
+for i = 1 : 19
+    fullpath{i} = fullfile(upperPath, relativePath, fname{i});
+end
+
+for i = 1 : 19
+    data = readtable(fullpath{i});
+% Извлечение значений моментов времени
+    t = table2array(data(:, 1));
+    t = t/3600;
+    y = table2array(data(:, end));
+% Построение графика
+    plot(t, y, 'LineWidth', 3, 'LineStyle', '--');
+    hold on;
+end
+
+fname = 'output physical.csv';
+fullpath = fullfile(upperPath, relativePath, fname);
+data = readtable(fullpath);
+% Извлечение значений моментов времени
+t = table2array(data(:, 1));
+t = t/3600;
+y = table2array(data(:, end));
+% Построение графика
+plot(t, y, 'LineWidth', 3, 'LineStyle', '--');
+hold on;
+
+graphname{20} = 'Физ. диффузия';
+
+
+
+filename_pd = 'physical_diffusion.txt';
+fullpath_3 = fullfile(upperPath, relativePath, filename_pd);
+t = load(fullpath_3);
+t = t/3600;
+xline(t(1), LineStyle=":", LineWidth=3, Color="r")
+
+xline(t(2), LineStyle=":", LineWidth=3, Color="r")
+graphname{21} = 'Границы физ. диффузии';
+
+
+legend(graphname)
+
+grid on
+
+xlim([80.5 81])
+
+ylim([848 864])
+
+xlabel('t, ч', 'FontSize',18)
+
+ylabel('\rho, кг/м^3', 'FontSize',18)
+
+saveas(gcf,'Part 3.png')
