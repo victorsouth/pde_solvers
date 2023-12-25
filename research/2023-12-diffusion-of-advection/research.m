7f9692ab2e656d6d88c65201e9b732f22d17b9b5
Егор Тараканчиков <egor200050@gmail.com>
2023-12-25 15:05:31 +0300

    refactor plotter

--- research/2023-12-diffusion-of-advection/research.m
+++ research/2023-12-diffusion-of-advection/research.m
@@ -1,86 +1,95 @@
-clc
-clear all
-currentDirectory = pwd;
+function main()
+    clc
+    clear
+    currentDirectory = pwd;
+    [upperPath, ~, ~] = fileparts(currentDirectory);
+    [upperPath, ~, ~] = fileparts(upperPath);
 
-[upperPath, ~, ~] = fileparts(currentDirectory);
-[upperPath, ~, ~] = fileparts(upperPath);
+    n = 0.05;
+    relativePath = fullfile('research_out', 'DiffusionOfAdvection', 'CompareQuickestDiffusion');
 
-n=0.05;
+    [fname, graphname, fullpath] = generateFileNames(upperPath, relativePath, n);
 
-relativePath = fullfile('research_out', 'DiffusionOfAdvection','CompareQuickestDiffusion');
+    plotGraphs(fullpath);
 
-for i = 1 : 19
-    n_str = num2str(n);
-    fname{i} = strcat('output Cr=',n_str,'.csv');
-    graphname{i} = strcat('Cr = ',n_str);
-    n = n + 0.05;
+    plotPhysicalDiffusion(upperPath, relativePath);
+
+    customizePlot(upperPath, relativePath, graphname); % Передаем upperPath и relativePath
 end
 
-for i = 1 : 19
-    fullpath{i} = fullfile(upperPath, relativePath, fname{i});
+function [fname, graphname, fullpath] = generateFileNames(upperPath, relativePath, n)
+    fname = cell(1, 19);
+    graphname = cell(1, 21);
+    fullpath = cell(1, 19);
+
+    for i = 1:19
+        n_str = num2str(n);
+        fname{i} = strcat('output Cr=', n_str, '.csv');
+        graphname{i} = strcat('Cr = ', n_str);
+        n = n + 0.05;
+    end
+
+    for i = 1:19
+        fullpath{i} = fullfile(upperPath, relativePath, fname{i});
+    end
+
+    fname{20} = 'output physical.csv';
+    fullpath{20} = fullfile(upperPath, relativePath, fname{20});
+    graphname{20} = 'Физ. диффузия';
+
+    fname{21} = 'physical_diffusion.txt';
+    fullpath{21} = fullfile(upperPath, relativePath, fname{21});
+    graphname{21} = 'Границы физ. диффузии';
 end
 
-for i = 1 : 19
-    data = readtable(fullpath{i});
-% Извлечение значений моментов времени
+function plotGraphs(fullpath)
+    figure;
+
+    for i = 1:19
+        data = readtable(fullpath{i});
+        plotData(data);
+    end
+
+    fname = 'output physical.csv';
+    data = readtable(fullpath{20});
+    plotData(data);
+
+
+    grid on;
+    xlim([80.5 81]);
+    ylim([848 864]);
+    xlabel('t, ч', 'FontSize', 18);
+    ylabel('\rho, кг/м^3', 'FontSize', 18);
+    hold off;
+end
+
+function plotData(data)
     t = table2array(data(:, 1));
-    t = t/3600;
+    t = t / 3600;
     y = table2array(data(:, end));
-% Построение графика
     plot(t, y, 'LineWidth', 3, 'LineStyle', '--');
     hold on;
 end
 
-fname = 'output physical.csv';
-fullpath = fullfile(upperPath, relativePath, fname);
-data = readtable(fullpath);
-% Извлечение значений моментов времени
-t = table2array(data(:, 1));
-t = t/3600;
-y = table2array(data(:, end));
-% Построение графика
-plot(t, y, 'LineWidth', 3, 'LineStyle', '--');
-hold on;
+function plotPhysicalDiffusion(upperPath, relativePath)
+    filename_pd = 'physical_diffusion.txt';
+    fullpath_3 = fullfile(upperPath, relativePath, filename_pd);
+    t = load(fullpath_3);
+    t = t / 3600;
+    xline(t(1), 'LineStyle', ':', 'LineWidth', 3, 'Color', 'r');
+    xline(t(2), 'LineStyle', ':', 'LineWidth', 3, 'Color', 'r'); 
 
-graphname{20} = 'Физ. диффузия';
+end
 
+function customizePlot(upperPath, relativePath, graphname)
+    fig = gcf;
+    fig.Position = [0, 0, 1920, 1080];
+    fig.Color = [1, 1, 1];
+    legend(graphname);
+    legend('show');
+    lgd = legend;
+    lgd.FontSize = 14;
 
-
-filename_pd = 'physical_diffusion.txt';
-fullpath_3 = fullfile(upperPath, relativePath, filename_pd);
-t = load(fullpath_3);
-t = t/3600;
-xline(t(1), LineStyle=":", LineWidth=3, Color="r")
-
-xline(t(2), LineStyle=":", LineWidth=3, Color="r")
-graphname{21} = 'Границы физ. диффузии';
-
-
-legend(graphname)
-
-grid on
-
-xlim([80.5 81])
-
-ylim([848 864])
-
-xlabel('t, ч', 'FontSize',18)
-
-ylabel('\rho, кг/м^3', 'FontSize',18)
-
-hold off
-
-% Настройка размера графика
-fig = gcf;
-fig.Position = [0, 0, 1920, 1080]; % [x, y, width, height]
-
-% Изменение цвета фона
-fig.Color = [1, 1, 1]; % Белый фон
-
-% Добавление легенды и увеличение ее масштаба
-legend('show');
-lgd = legend;
-lgd.FontSize = 14; % Размер шрифта
-fullpath = fullfile(upperPath, relativePath, 'Part 3.png');
-
-saveas(gcf, fullpath)
+    fullpath = fullfile(upperPath, relativePath, 'Part 3.png');
+    saveas(gcf, fullpath);
+end
