f812ba91153e25e670568a74c8d671026bf1c8b1
madvln <128503756+madvln@users.noreply.github.com>
2024-03-07 15:53:05 +0300

    Quick with quasistationary model (#37)
    * first try to make working QSM with quickest ult
    
    * try 2
    
    * try 3, success
    
    * added euler pde
    
    * working quick?
    
    * Finally working QSM with quickest_ultimate
    
    * try to make pp task
    
    * utf-8
    
    * refactoring part 1
    
    * fixed euler
    
    * refactoring part 2
    
    * added test for researching density_viscosity_layer_for_quick
    
    * TimeRowsTask coming back
    
    * working wrapper for quickest_ult
    
    * - ugly test
    
    * big cleanup
    
    * little fixies
    
    * fix: buffer changes too
    
    * refactor: Враппер для квивеста
    
    * clean up and commentaries
    
    * commentaries on wrapper & QU solver
    
    ---------
    
    Co-authored-by: Егор Тараканчиков <egor200050@gmail.com>
    Co-authored-by: Victor Yuzhanin <yuzhanin.v@gubkin.ru>

---
+++ research/2024-02-quick-with-quasistationary-model/quick_with_quasistationary_model.h
@@ -0,0 +1,98 @@
+﻿#pragma once
+
+/// @brief Тесты для солвера quickest_ultimate_fv_solver
+class QuickWithQuasiStationaryModel : public ::testing::Test {
+protected:
+
+protected:
+    /// @brief Параметры трубы
+    pipe_properties_t pipe;
+    /// @brief Профиль расхода
+    vector<double> Q_profile;
+    /// @brief Модель адвекции
+    std::unique_ptr<PipeQAdvection> advection_model;
+protected:
+
+    /// @brief Подготовка к расчету для семейства тестов
+    virtual void SetUp() override {
+        // Упрощенное задание трубы - 200км, с шагом разбиения для расчтной сетки 100 м, диаметром 514мм
+        simple_pipe_properties simple_pipe;
+        simple_pipe.length = 200e3;
+        simple_pipe.diameter = 0.514;
+        simple_pipe.dx = 100;
+
+        pipe = pipe_properties_t::build_simple_pipe(simple_pipe);
+        Q_profile = vector<double>(pipe.profile.getPointCount(), 0.2); // задаем по трубе расход 0.2 м3/с
+        advection_model = std::make_unique<PipeQAdvection>(pipe, Q_profile);
+    }
+};
+
+/// @brief Слой для расчета плотности, вязкости методом конечных объемов 
+struct density_viscosity_cell_layer {
+    /// @brief Профиль плотности
+    std::vector<double> density;
+    /// @brief Профиль вязкости
+    std::vector<double> viscosity;
+    /// @brief Профиль вспомогательных расчетов для метода конечных объемов (и для вязкости, и для плотности)
+    quickest_ultimate_fv_solver_traits<1>::specific_layer specific;
+    /// @brief Инициализация профилей
+    /// @param point_count Количество точек
+    density_viscosity_cell_layer(size_t point_count)
+        : density(point_count - 1)
+        , viscosity(point_count - 1)
+        , specific(point_count)
+    {
+    }
+
+    // @brief Подготовка плотности для расчета методом конечных объемов 
+    static quickest_ultimate_fv_wrapper<1> get_density_quick_wrapper(density_viscosity_cell_layer& layer)
+    {
+        return quickest_ultimate_fv_wrapper<1>(layer.density, layer.specific);
+    }
+    /// @brief Подготовка вязкости для расчета методом конечных объемов 
+    static quickest_ultimate_fv_wrapper<1> get_viscosity_quick_wrapper(density_viscosity_cell_layer& layer)
+    {
+        return quickest_ultimate_fv_wrapper<1>(layer.viscosity, layer.specific);
+    }
+};
+
+/// @brief Базовый пример использования метода Quickest Ultimate для уравнения адвекции
+TEST_F(QuickWithQuasiStationaryModel, UseCase_Advection_Density_Viscosity)
+{
+    const auto& x = advection_model->get_grid();
+    double dx = x[1] - x[0];
+    double v = advection_model->getEquationsCoeffs(0, 0);
+
+    ring_buffer_t<density_viscosity_cell_layer> buffer(2, pipe.profile.getPointCount());
+
+    auto& rho_initial = buffer[0].density;
+    auto& viscosity_initial = buffer[0].viscosity;
+    rho_initial = vector<double>(rho_initial.size(), 850); // инициализация начальной плотности
+    viscosity_initial = vector<double>(viscosity_initial.size(), 1e-5); // инициализация начальной плотности
+    
+    buffer.advance(+1);
+    
+    {
+        auto density_wrapper = buffer.get_buffer_wrapper(
+            &density_viscosity_cell_layer::get_density_quick_wrapper);
+        quickest_ultimate_fv_solver solver(*advection_model, density_wrapper);
+
+        double dt = abs(dx / v);
+        double rho_in = 840; // плотность нефти, закачиваемой на входе трубы при положительном расходе
+        double rho_out = 860; // плотность нефти, закачиваемой с выхода трубы при отрицательном расходе
+        solver.step(dt, rho_in, rho_out);
+    }
+
+    {
+        auto viscosity_wrapper = buffer.get_buffer_wrapper(
+            &density_viscosity_cell_layer::get_viscosity_quick_wrapper);
+        quickest_ultimate_fv_solver solver(*advection_model, viscosity_wrapper);
+
+        double dt = abs(dx / v);
+        double visc_in = 2e-5; // вязкость нефти, закачиваемой на входе трубы при положительном расходе
+        double visc_out = 0.5e-5;; // вязкость нефти, закачиваемой с выхода трубы при отрицательном расходе
+        solver.step(dt, visc_in, visc_out);
+    }
+
+    auto& curr = buffer[0];
+}
