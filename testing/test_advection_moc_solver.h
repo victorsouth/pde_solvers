186821f3bca36ea70b2653783e604b95c4e73e1a
alexbibich <2marussia2@gmail.com>
2024-03-29 23:03:29 +0300

    Исправил замечания и перевёл на TEST_F

--- testing/test_advection_moc_solver.h
+++ testing/test_advection_moc_solver.h
@@ -1,30 +1,50 @@
 ﻿#pragma once
 
-/// @brief Пример квазистационарного расчёта трубопровода для плотности
-TEST(TransportMocSolver, UseCaseDensity)
+/// @brief Класс для тестов advection_moc_solver
+class AdvectionMocSolver : public ::testing::Test
 {
-    // Зададимся исходными данными
+public:
+    /// @brief Подготовка модели трубопровода 
+    static pipe_properties_t PrepareTestPipe()
+    {
+        // Зададимся исходными данными
+        pipe_properties_t pipe;
+        double x0 = 0;
+        double xl = 1e5;
+        double d = 1;
+        size_t n = 100;
+
+        pipe.profile = PipeProfile::create(n, x0, xl, 0, 0, 10e6);
+        pipe.wall.diameter = d;
+
+        return pipe;
+    }
+
+protected:
+    // Модель трубопровода
     pipe_properties_t pipe;
-    double x0 = 0;
-    double xl = 1e5;
-    double d = 1;
-    size_t n = 100;
+    // Исходные данные
     double rho_init = 850;
     double rho_left = 870;
     double rho_right = 830;
-    double volumetric_flow = 0.5;
     double T = 1000;
 
-    // Модель трубопровода
-    pipe.profile = PipeProfile::create(n, x0, xl, 0, 0, 10e6);
-    pipe.wall.diameter = d;
+    virtual void SetUp() override {
+        pipe = PrepareTestPipe();
+    };
+};
+
+/// @brief Пример квазистационарного расчёта трубопровода для плотности
+TEST_F(AdvectionMocSolver, UseCaseDensity)
+{
+    // Зададимся расходом внутри трубопровода
+    double volumetric_flow = 0.5;
 
     // Буфер для хранения слоёв
     ring_buffer_t<vector<double>> buffer(2, pipe.profile.getPointCount());
     buffer.previous() = vector<double>(pipe.profile.getPointCount(), rho_init);
 
     double modeling_time = 0;
-
 
     while (modeling_time <= T)
     {
@@ -41,28 +61,15 @@ TEST(TransportMocSolver, UseCaseDensity)
 }
 
 /// @brief Проверка на то, правильно ли учитывается инверсия потока 
-TEST(TransportMocSolver, CanConsiderFlowSwap)
+TEST_F(AdvectionMocSolver, ConsiderFlowSwap)
 {
-    // Зададимся исходными данными
-    pipe_properties_t pipe;
-    double x0 = 0;
-    double xl = 1e5;
-    double d = 1;
-    size_t n = 100;
-    double rho_init = 850;
-    double rho_left = 870;
-    double rho_right = 830;
+    // Зададимся расходом внутри трубопровода для инверсии потока
     double volumetric_flow = -0.5;
 
     // Буфер для хранения слоёв
-    pipe.profile = PipeProfile::create(n, x0, xl, 0, 0, 10e6);
-    pipe.wall.diameter = d;
-
-    // Модель трубопровода
     ring_buffer_t<vector<double>> buffer(2, pipe.profile.getPointCount());
     buffer.previous() = vector<double>(pipe.profile.getPointCount(), rho_init);
     
-
     advection_moc_solver solver(pipe, volumetric_flow, buffer.previous(), buffer.current());
 
     solver.step(rho_left, rho_right);
