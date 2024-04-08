da51c044e49dc19533c049d68cb2fe39779f43d5
Егор Тараканчиков <egor200050@gmail.com>
2024-04-08 15:10:16 +0300

    research commit 2024-04-08

--- research/2024-02-quick-with-quasistationary-model/quick_with_quasistationary_model.h
+++ research/2024-02-quick-with-quasistationary-model/quick_with_quasistationary_model.h
@@ -151,6 +151,31 @@ protected:
     }
 };
 
+/// @brief Тесты для солвера moc_solver
+class MocWithQuasiStationaryModel : public ::testing::Test {
+protected:
+    /// @brief Параметры трубы
+    pipe_properties_t pipe;
+    /// @brief Профиль расхода
+    vector<double> Q_profile;
+    /// @brief Модель адвекции
+    std::unique_ptr<PipeQAdvection> advection_model;
+protected:
+
+    /// @brief Подготовка к расчету для семейства тестов
+    virtual void SetUp() override {
+        // Упрощенное задание трубы - 200км, с шагом разбиения для расчтной сетки 100 м, диаметром 514мм
+        simple_pipe_properties simple_pipe;
+        simple_pipe.length = 200e3;
+        simple_pipe.diameter = 0.514;
+        simple_pipe.dx = 100;
+
+        pipe = pipe_properties_t::build_simple_pipe(simple_pipe);
+        Q_profile = vector<double>(pipe.profile.getPointCount(), 0.2); // задаем по трубе расход 0.2 м3/с
+        advection_model = std::make_unique<PipeQAdvection>(pipe, Q_profile);
+    }
+};
+
 /// @brief Слой для расчета плотности, вязкости методом конечных объемов 
 struct density_viscosity_cell_layer {
     /// @brief Профиль плотности
@@ -174,14 +199,51 @@ struct density_viscosity_cell_layer {
     {}
 
     // @brief Подготовка плотности для расчета методом конечных объемов 
-    static quickest_ultimate_fv_wrapper<1> get_density_quick_wrapper(density_viscosity_cell_layer& layer)
+    static quickest_ultimate_fv_wrapper<1> get_density_wrapper(density_viscosity_cell_layer& layer)
     {
         return quickest_ultimate_fv_wrapper<1>(layer.density, layer.specific);
     }
     /// @brief Подготовка вязкости для расчета методом конечных объемов 
-    static quickest_ultimate_fv_wrapper<1> get_viscosity_quick_wrapper(density_viscosity_cell_layer& layer)
+    static quickest_ultimate_fv_wrapper<1> get_viscosity_wrapper(density_viscosity_cell_layer& layer)
     {
         return quickest_ultimate_fv_wrapper<1>(layer.viscosity, layer.specific);
+    }
+};
+
+/// @brief Проблемно-ориентированный слой
+/// Задача про плотность и вязкость
+struct density_viscosity_layer_moc
+{
+    /// @brief Профиль плотности
+    std::vector<double> density;
+    /// @brief Профиль вязкости
+    std::vector<double> viscosity;
+    /// @brief Профиль давления
+    std::vector<double> pressure;
+    /// @brief Дифференциальный профиль давления
+    std::vector<double> pressure_delta;
+    /// @brief Профиль вспомогательных расчетов для МХ (и для вязкости, и для плотности)
+    moc_solver<1>::specific_layer specific;
+    /// @brief Конструктор на заданное количество точек
+    density_viscosity_layer_moc(size_t point_count)
+        : density(point_count)
+        , viscosity(point_count)
+        , specific(point_count)
+        , pressure(point_count)
+        , pressure_delta(point_count)
+    {
+
+    }
+    /// @brief Подготовка плотности для расчета по методу характеристик
+    /// Оборачивает профиль плотности и вспомогательный расчет МХ в обертку для МХ
+    static moc_layer_wrapper<1> get_density_wrapper(density_viscosity_layer_moc& layer)
+    {
+        return moc_layer_wrapper<1>(layer.density, layer.specific);
+    }
+    /// @brief Подготовка вязкости для расчета методом характеристик moc_solver
+    static moc_layer_wrapper<1> get_viscosity_wrapper(density_viscosity_layer_moc& layer)
+    {
+        return moc_layer_wrapper<1>(layer.viscosity, layer.specific);
     }
 };
 
@@ -247,12 +309,36 @@ public:
         return result;
     }
 };
+
+/// @brief Формирует имя файл для результатов исследования разных численных метов
+/// @tparam Solver Класс солвера
+/// @param path Путь, в котором формируется файл
+/// @param Cr Число куранта
+/// @return Путь к файлу в заивисимости от указанного класса солвера
+template <typename Solver>
+static std::string get_courant_research_filename(const string& path)
+{
+    std::stringstream filename;
+    if constexpr (std::is_same<Solver, quickest_ultimate_fv_solver>::value) {
+        filename << path << "output ultimate"<< ".csv";
+    }
+    else if constexpr (std::is_same<Solver, moc_solver<1>>::value)
+    {
+        filename << path << "output MOC"<< ".csv";
+    }
+    else {
+        throw std::runtime_error("Please specify filename for solver type");
+    }
+    return filename.str();
+}
+
 /// @brief Структура, созданная для хранения в себе начального профиля давлений и буфера с расчетными данными
+template <typename Layer>
 struct task_buffer_t {
     /// @brief Изначальный профиль давления
     vector<double> pressure_initial;
     /// @brief Буфер профилей давления, плотности, вязкости
-    ring_buffer_t<density_viscosity_cell_layer> buffer;
+    ring_buffer_t<Layer> buffer;
     task_buffer_t(size_t point_count)
         : pressure_initial(point_count)
         , buffer(2, point_count)
@@ -280,9 +366,10 @@ struct quasistatic_task_boundaries_t {
     }
 };
 /// @brief Проблемно-ориентированный класс для задачи PQ
+template <typename Layer, typename Solver>
 class quasistatic_task_t {
     pipe_properties_t pipe;
-    task_buffer_t buffer;
+    task_buffer_t<Layer> buffer;
 
 public:
     quasistatic_task_t(const pipe_properties_t& pipe,
@@ -294,8 +381,15 @@ public:
 
         // Инициализация реологии
         auto& current = buffer.buffer.current();
-        current.density = vector<double>(n - 1, initial_conditions.density); // Инициализация начального профиля плотности
-        current.viscosity = vector<double>(n - 1, initial_conditions.viscosity); // Инициализация начального профиля вязкости
+        if constexpr (std::is_same<Solver, moc_solver<1>>::value) {
+            current.density = vector<double>(n, initial_conditions.density); // Инициализация начального профиля плотности
+            current.viscosity = vector<double>(n, initial_conditions.viscosity); // Инициализация начального профиля вязкости
+        }
+        else {
+            current.density = vector<double>(n - 1, initial_conditions.density); // Инициализация начального профиля плотности
+            current.viscosity = vector<double>(n - 1, initial_conditions.viscosity); // Инициализация начального профиля вязкости
+        }
+
 
         // Начальный гидравлический расчет
         int euler_direction = +1;
@@ -319,12 +413,15 @@ public:
         auto& current = buffer.buffer.current();
 
         vector<double>& p_profile = current.pressure;
-
+        vector<double> density_buffer;
+        vector<double> viscosity_buffer;
         auto density_wrapper = buffer.buffer.get_buffer_wrapper(
-            &density_viscosity_cell_layer::get_density_quick_wrapper);
+            &Layer::get_density_wrapper);
 
         auto viscosity_wrapper = buffer.buffer.get_buffer_wrapper(
-            &density_viscosity_cell_layer::get_viscosity_quick_wrapper);
+            &Layer::get_viscosity_wrapper);
+
+
 
         int euler_direction = +1; // Задаем направление для Эйлера
 
@@ -332,14 +429,30 @@ public:
         vector<double>Q_profile(n, boundaries.volumetric_flow); // задаем по трубе новый расход из временного ряда
 
         PipeQAdvection advection_model(pipe, Q_profile);
-        // Шаг по плотности
-        quickest_ultimate_fv_solver solver_rho(advection_model, density_wrapper);
-        solver_rho.step(dt, boundaries.density, boundaries.density);
-        // Шаг по вязкости
-        quickest_ultimate_fv_solver solver_nu(advection_model, viscosity_wrapper);
-        solver_nu.step(dt, boundaries.viscosity, boundaries.viscosity);
+        
+        if constexpr (std::is_same<Solver, moc_solver<1>>::value) {
+            // Шаг по плотности
+            Solver solver_rho(advection_model, density_wrapper);
+            solver_rho.step_optional_boundaries(dt, boundaries.density, boundaries.density);
+            // Шаг по вязкости
+            Solver solver_nu(advection_model, viscosity_wrapper);
+            solver_nu.step_optional_boundaries(dt, boundaries.viscosity, boundaries.viscosity);
+            density_buffer = density_wrapper.current().values;
+            viscosity_buffer = viscosity_wrapper.current().values;
+        }
+        else {
+            // Шаг по плотности
+            Solver solver_rho(advection_model, density_wrapper);
+            solver_rho.step(dt, boundaries.density, boundaries.density);
+            // Шаг по вязкости
+            Solver solver_nu(advection_model, viscosity_wrapper);
+            solver_nu.step(dt, boundaries.viscosity, boundaries.viscosity);
+            density_buffer = density_wrapper.current().vars;
+            viscosity_buffer = viscosity_wrapper.current().vars;
+        }
 
-        pipe_model_PQ_cell_parties_t pipeModel(pipe, density_wrapper.current().vars, viscosity_wrapper.current().vars, Q_profile[0], euler_direction);
+
+        pipe_model_PQ_cell_parties_t pipeModel(pipe, density_buffer, viscosity_buffer, Q_profile[0], euler_direction);
         // Получаем новый профиль давлений
         solve_euler<1>(pipeModel, euler_direction, boundaries.pressure_in, &p_profile);
         // Получаем дифференциальный профиль давлений
@@ -360,6 +473,18 @@ public:
 
 };
 
+inline std::string prepare_research_folder_for_qsm_model()
+{
+    auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    std::string research_name = std::string(test_info->test_case_name());
+    std::string case_name = std::string(test_info->name());
+
+    std::string path = std::string("../research_out/QSM_models/") +
+        research_name + "/" + case_name + "/";
+    std::filesystem::create_directories(path);
+    return path;
+}
+
 /// @brief Базовый пример использования метода Quickest Ultimate для уравнения адвекции
 TEST_F(QuickWithQuasiStationaryModel, UseCase_Advection_Density_Viscosity)
 {
@@ -378,7 +503,7 @@ TEST_F(QuickWithQuasiStationaryModel, UseCase_Advection_Density_Viscosity)
 
     {
         auto density_wrapper = buffer.get_buffer_wrapper(
-            &density_viscosity_cell_layer::get_density_quick_wrapper);
+            &density_viscosity_cell_layer::get_density_wrapper);
         quickest_ultimate_fv_solver solver(*advection_model, density_wrapper);
 
         double dt = abs(dx / v);
@@ -389,7 +514,7 @@ TEST_F(QuickWithQuasiStationaryModel, UseCase_Advection_Density_Viscosity)
 
     {
         auto viscosity_wrapper = buffer.get_buffer_wrapper(
-            &density_viscosity_cell_layer::get_viscosity_quick_wrapper);
+            &density_viscosity_cell_layer::get_viscosity_wrapper);
         quickest_ultimate_fv_solver solver(*advection_model, viscosity_wrapper);
 
         double dt = abs(dx / v);
@@ -400,7 +525,7 @@ TEST_F(QuickWithQuasiStationaryModel, UseCase_Advection_Density_Viscosity)
 
     auto& curr = buffer[0];
 }
-
+/// @brief Базовый пример генерации синтетических временных рядов
 TEST(Random, PrepareTimeSeries)
 {
     timeseries_generator_settings settings;
@@ -437,22 +562,73 @@ TEST(Random, PrepareTimeSeries)
     // Интерополируем значения параметров в заданный момент времени
     vector<double> values_in_test_time = params(test_time);
 }
-
+/// @brief Пример испольования метода Quickest Ultimate с гидравлическим расчетом  
 TEST_F(QuickWithQuasiStationaryModel, WorkingWithTimeSeries)
 {
     // Объявляем структуру с исходными данными и настроечными параметрами
     timeseries_generator_settings settings = timeseries_generator_settings::default_values();
     // Генерируем данные
     synthetic_time_series_generator data_time_series(settings);
+
+    const time_t jump_time_Q = 100000;
+    const double jump_value_Q = 0.1;
+    data_time_series.apply_jump(jump_time_Q, jump_value_Q, "Q");
+
     // Получаем данные
     const auto& data = data_time_series.get_data();
     // Помещаем временные ряды в вектор
     vector_timeseries_t params(data);
 
-    task_buffer_t buffer(pipe.profile.getPointCount());
+    task_buffer_t<density_viscosity_cell_layer> buffer(pipe.profile.getPointCount());
 
     quasistatic_task_boundaries_t initial_boundaries = { 0.2, 6e6, 850, 15e-6 };
-    quasistatic_task_t task(pipe, initial_boundaries);
+    quasistatic_task_t<density_viscosity_cell_layer, quickest_ultimate_fv_solver> task(pipe, initial_boundaries);
+
+    task.advance();
+
+    double v_max = 1; // Предполагаем скорость для Куранта = 1, скорость, больше чем во временных рядах и в профиле
+    time_t dt = task.get_time_step_assuming_max_speed(v_max);
+    time_t t = std::time(nullptr); // Момент времени начала моделирования
+
+    do
+    {
+        t += dt;
+        // Интерополируем значения параметров в заданный момент времени
+        vector<double> values_in_time_model = params(t);
+
+        quasistatic_task_boundaries_t boundaries;
+        boundaries.volumetric_flow = values_in_time_model[0];
+        boundaries.pressure_in = values_in_time_model[1];
+        boundaries.density = values_in_time_model[2];
+        boundaries.viscosity = values_in_time_model[3];
+
+        task.step(dt, boundaries);
+        task.advance();
+        auto& rho_profile_1 = task.get_buffer().current().density;
+        auto& rho_profile_2 = task.get_buffer().previous().density;
+    } while (t < std::time(nullptr) + settings.duration - dt);
+}
+/// @brief Пример испольования метода характеристик с гидравлическим расчетом  
+TEST_F(MocWithQuasiStationaryModel, WorkingWithTimeSeries)
+{
+    // Объявляем структуру с исходными данными и настроечными параметрами
+    timeseries_generator_settings settings = timeseries_generator_settings::default_values();
+    // Генерируем данные
+    synthetic_time_series_generator data_time_series(settings);
+
+    const time_t jump_time_Q = 100000;
+    const double jump_value_Q = 0.1;
+    data_time_series.apply_jump(jump_time_Q, jump_value_Q, "Q");
+
+    // Получаем данные
+    const auto& data = data_time_series.get_data();
+    // Помещаем временные ряды в вектор
+    vector_timeseries_t params(data);
+
+    task_buffer_t<density_viscosity_layer_moc> buffer(pipe.profile.getPointCount());
+
+    quasistatic_task_boundaries_t initial_boundaries = { 0.2, 6e6, 850, 15e-6 };
+    quasistatic_task_t<density_viscosity_layer_moc, moc_solver<1>> task(pipe, initial_boundaries);
 
     task.advance();
 
