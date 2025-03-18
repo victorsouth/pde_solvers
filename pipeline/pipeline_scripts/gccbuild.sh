99476d44087dd889633e88ebeaf91c390f02c4d5
Shtrigel.Dmitry <Shtrigel@SHTRIGELDMITRY>
2025-03-18 10:12:11 +0300

    (+) Добавлен флаг сборки тестов

--- pipeline/pipeline_scripts/gccbuild.sh
+++ pipeline/pipeline_scripts/gccbuild.sh
@@ -11,7 +11,7 @@ mkdir -p ${LOG_DIR}
 mkdir -p build
 cd build 
 #Собираем pde_solvers
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w -Wno-#pragma-messages" -DCMAKE_INSTALL_PREFIX=../Libs/GCC > ${LOG_DIR}/pde_solvers_build.log 2>&1
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w -Wno-#pragma-messages" -DPDE_SOLVERS_BUILD_TESTS=ON -DCMAKE_INSTALL_PREFIX=../Libs/GCC > ${LOG_DIR}/pde_solvers_build.log 2>&1
 make -j$(nproc) >> ${LOG_DIR}/pde_solvers_build.log 2>&1
 MAKE_RESULT=$?
 # Выводим ошибки из файла pde_solvers_build.log, содержащие 'error:' или 'fatal error:'
