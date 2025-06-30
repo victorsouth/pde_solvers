f6dcbbc07d28f6264fd34e64752fc871a7d7057e
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2025-06-30 16:53:19 +0300

    refactor: remove usings

--- pde_solvers/timeseries/csv_readers.h
+++ pde_solvers/timeseries/csv_readers.h
@@ -1,13 +1,5 @@
 ﻿#pragma once
 
-#include <fstream>
-#include "timeseries_helpers.h" 
-
-
-using std::pair;
-using std::string;
-using std::vector;
-
 /// @brief Информация о теге временного ряда (имя, размерность)
 struct tag_info_t {
     /// @brief Имя тега
@@ -25,14 +17,14 @@ public:
     /// @param time_begin Начало периода
     /// @param time_end Конец периода
     /// @return Временной ряд в формате пары векторов: [Метки времени; Значения параметра]
-    static pair<vector<time_t>, vector<double>> read_from_stream(std::istream& input_stream, const string& dimension, time_t time_begin = std::numeric_limits<time_t>::min(),
+    static std::pair<std::vector<time_t>, std::vector<double>> read_from_stream(std::istream& input_stream, const std::string& dimension, time_t time_begin = std::numeric_limits<time_t>::min(),
         time_t time_end = std::numeric_limits<time_t>::max())
     {
-        vector<time_t> t;
-        vector<double> x;
+        std::vector<time_t> t;
+        std::vector<double> x;
 
-        string line_from_file;
-        vector<string> split_line_to_file;
+        std::string line_from_file;
+        std::vector<std::string> split_line_to_file;
         while (getline(input_stream, line_from_file))
         {
             split_line_to_file = split_str(line_from_file, ';');
@@ -65,7 +57,7 @@ private:
     /// @param time_begin Начало периода
     /// @param time_end Конец периода
     /// @return Временной ряд в формате пары векторов: [Метки времени; Значения параметра]
-    static pair<vector<time_t>, vector<double>> read_from_file(const string& filename, const string& dimension, time_t time_begin = std::numeric_limits<time_t>::min(),
+    static std::pair<std::vector<time_t>, std::vector<double>> read_from_file(const std::string& filename, const std::string& dimension, time_t time_begin = std::numeric_limits<time_t>::min(),
         time_t time_end = std::numeric_limits<time_t>::max())
     {
         std::ifstream infile(filename);
@@ -77,13 +69,13 @@ private:
 public:
     /// @brief Конструктор
     /// @param data Название тега и инструкция перевода единиц измерения
-    csv_tag_reader(const pair<string, string>& data)
+    csv_tag_reader(const std::pair<std::string, std::string>& data)
         : csv_tag_reader(data.first, data.second)
     { }
     /// @brief Конструтор
     /// @param tag Имя тега
     /// @param dimension Инструкция для перевода единиц измерения
-    csv_tag_reader(const string& tag, const string& dimension)
+    csv_tag_reader(const std::string& tag, const std::string& dimension)
         : tagname{ tag }, dim{ dimension }
     { }
     /// @brief Конструктор
@@ -96,7 +88,7 @@ public:
     /// @param unixtime_to Конец периода задаётся строкой формата dd:mm:yyyy HH:MM:SS
     /// @return возвращает временной ряд в формате 
     /// для хранения в vector_timeseries_t
-    pair<vector<time_t>, vector<double>> read_csv(const string& unixtime_from, const string& unixtime_to) const
+    std::pair<std::vector<time_t>, std::vector<double>> read_csv(const std::string& unixtime_from, const std::string& unixtime_to) const
     {
         time_t start_period = StringToUnix(unixtime_from);
         time_t end_period = StringToUnix(unixtime_to);
@@ -109,13 +101,13 @@ public:
     /// @param end_period Конец периода задаётся типом данных time_t
     /// @return возвращает временной ряд в формате 
     /// для хранения в vector_timeseries_t
-    pair<vector<time_t>, vector<double>> read_csv(
+    std::pair<std::vector<time_t>, std::vector<double>> read_csv(
         time_t start_period = std::numeric_limits<time_t>::min(),
         time_t end_period = std::numeric_limits<time_t>::max()) const
     {
-        pair<vector<time_t>, vector<double>> data;
+        std::pair<std::vector<time_t>, std::vector<double>> data;
 
-        string extension = ".csv";
+        std::string extension = ".csv";
 
         data = read_from_file(tagname + extension, dim, start_period, end_period);
 
@@ -124,8 +116,8 @@ public:
 
 private:
     /// @brief Название тега и инструкция перевода единиц измерения
-    const string tagname;
-    const string dim;
+    const std::string tagname;
+    const std::string dim;
 };
 
 /// @brief Чтение параметров из исторических данных для нескольких тегов
@@ -141,7 +133,7 @@ public:
     /// @brief Конструктор
     /// @param data Вектор пар, в которых первый элемент название тега, 
     /// а второй - инструкция для перевода единиц измерения
-    csv_multiple_tag_reader(const vector<pair<string, string>>& tag_list)
+    csv_multiple_tag_reader(const std::vector<std::pair<std::string, std::string>>& tag_list)
     {
         for (const std::pair<std::string, std::string>& tag : tag_list)
         {
@@ -174,7 +166,7 @@ public:
     /// @param unixtime_to Конец периода задаётся строкой формата dd:mm:yyyy HH:MM:SS
     /// @return возвращает временной ряд в формате 
     /// для хранения в vector_timeseries_t
-    vector<pair<vector<time_t>, vector<double>>> read_csvs(const string& unixtime_from, const string& unixtime_to) const
+    std::vector<std::pair<std::vector<time_t>, std::vector<double>>> read_csvs(const std::string& unixtime_from, const std::string& unixtime_to) const
     {
         time_t start_period = StringToUnix(unixtime_from);
         time_t end_period = StringToUnix(unixtime_to);
@@ -187,17 +179,17 @@ public:
     /// @param end_period Конец периода задаётся типом данных time_t
     /// @return возвращает временной ряд в формате 
     /// для хранения в vector_timeseries_t
-    vector<pair<vector<time_t>, vector<double>>> read_csvs(
+    std::vector<std::pair<std::vector<time_t>, std::vector<double>>> read_csvs(
         time_t start_period = std::numeric_limits<time_t>::min(),
         time_t end_period = std::numeric_limits<time_t>::max()) const
     {
-        vector<pair<vector<time_t>, vector<double>>> data(filename_dim.size());
+        std::vector<std::pair<std::vector<time_t>, std::vector<double>>> data(filename_dim.size());
 
 #pragma omp parallel for schedule(dynamic)
         for (int i = 0; i < static_cast<int>(filename_dim.size()); i++)
         {
             csv_tag_reader tag_reader(filename_dim[i]);
-            pair<vector<time_t>, vector<double>> data_from_single_csv =
+            std::pair<std::vector<time_t>, std::vector<double>> data_from_single_csv =
                 tag_reader.read_csv(start_period, end_period);
             data[i] = std::move(data_from_single_csv);
         }
