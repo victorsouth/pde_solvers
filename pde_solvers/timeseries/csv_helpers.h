95a4ed51f70bcec75722b4d3d8a701de602f1c99
grishuhin.g <grishuhin.g@PC-T19>
2025-09-23 15:01:13 +0300

    feat: комменты

--- pde_solvers/timeseries/csv_helpers.h
+++ pde_solvers/timeseries/csv_helpers.h
@@ -3,7 +3,16 @@
 #include <unordered_map>
 #include <filesystem>
 
-
+/// @brief На основе исходного файла временных рядов в формате csv формирует файл, содержащий значения, преобразованные согласно правилу conversion_map. Преобразованные 
+/// временные ряды записываются в файл с постфиксом _converted. Временные метки остаются без изменений. 
+/// @warning Если в исходных данных содержится значение, которое преобразуется в exception_value, то создает исключение. Это полезно для
+/// обнаружения периодов некорректных данных (например, когда технологический объект был в аварийном состоянии)
+/// @tparam DataTypeOriginal Исходный тип данных из файла
+/// @tparam DataTypeConverted Целевой тип данных после конвертации
+/// @param filename Путь к исходному файлу
+/// @param conversion_map Карта сопоставления
+/// @param exception_value Значение для генерации исключения
+/// @return Путь к файлу со сконвертированными временными рядами
 template <typename DataTypeOriginal, typename DataTypeConverted>
 inline std::string convert_csv_values(const std::string& filename,
     const std::unordered_map<DataTypeOriginal, DataTypeConverted>& conversion_map,
